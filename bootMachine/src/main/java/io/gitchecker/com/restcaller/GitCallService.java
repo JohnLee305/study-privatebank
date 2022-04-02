1c7e826ccdc53b218cebda038fbc56609054c4e7
JohnLee305 <johnathanl305@gmail.com>
2022-04-02 14:32:47 +0900

    2022-04-02 깃허브 체커 git caller 개발중

--- bootMachine/src/main/java/io/gitchecker/com/restcaller/GitCallService.java
+++ bootMachine/src/main/java/io/gitchecker/com/restcaller/GitCallService.java
@@ -2,5 +2,23 @@ package io.gitchecker.com.restcaller;
 //TODO : 깃허브 API REST 통신하는 객체
 // 여기서는 GitInfoEntity 받아와서 rest 통신해서 다시 건네주는 역할만 해야하고, 얘는 그냥 빈으로 관리해서 싱글톤으로 갖고있는게 맞다.
 
+import io.gitchecker.com.gitCheckManager.entity.GitInfoEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
 public class GitCallService {
+
+   private static RestTemplate restTemplate;
+
+   // public GitInfoEntity callGithubUserCommitStatus(){
+
+    public GitInfoEntity callGithubUserCommitStatus(){
+
+    }
+
+
+
+
 }
