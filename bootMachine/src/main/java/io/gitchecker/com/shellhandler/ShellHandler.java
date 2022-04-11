1d94f701f385d620957904b364ca7107b81fa3b1
JohnLee305 <johnathanl305@gmail.com>
2022-04-11 23:39:06 +0900

    2022-04-11 커밋하는 쉘 스크립트 작성

--- bootMachine/src/main/java/io/gitchecker/com/shellhandler/ShellHandler.java
+++ bootMachine/src/main/java/io/gitchecker/com/shellhandler/ShellHandler.java
@@ -6,7 +6,10 @@ package io.gitchecker.com.shellhandler;
 
 //깃허브 커밋에 대한 순서 :
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * 준비물 :
@@ -14,7 +17,11 @@ import java.io.InputStream;
  * - 서버가 실행될 로컬 환경에 리포지토리 개발환경이 세팅되어있어야함.
  * - 비공개 리포지토리에 MD파일을 통하여 커밋정보 입력.
  * - 날짜 및 형식 template을 통하여 일일단위의 입력
- * 실행순서 :
+ * - 정해진 순서의 쉘 스크립트
+ * - 쉘 스크립트의 실행 권한 부여 여부 확인 (chmod +x gitScript.sh)
+ *
+ *
+ * 실행순서 : (쉘 스크립팅이 필요할것같다.)
  * git pull 받는다.
  * readme.md 파일을 읽어들인다.
  * readme.md 파일을 수정한다 (템플릿의 형식에 맞게 추가작성)
@@ -26,8 +33,6 @@ import java.io.InputStream;
 public class ShellHandler {
     private InputStream inputStream;
 
-
-
     //실행하는 OS판별
     // mac os x : 개발
     // windows : 예외처리
@@ -37,16 +42,44 @@ public class ShellHandler {
                 .toLowerCase().startsWith("windows");
 
         System.out.println("실행환경이 윈도우인가? " + isWindows);
-        System.out.println("그렇다면? "+ System.getProperty("os.name")
-                .toLowerCase());
-        return System.getProperty("os.name")
-                .toLowerCase();
+        System.out.println("그렇다면? "+ System.getProperty("os.name").toLowerCase());
+        return System.getProperty("os.name").toLowerCase();
     }
 
 
     public static void main(String[] args) {
+        String filePath = "/Users/leejeonghoon/Desktop/DevStud/hubchecker";
+
         ShellHandler shellHandler = new ShellHandler();
         shellHandler.checkOSName();
+
+        //OS 이름에 따른 경로 명 변경(profile 분리 필요하다면 확장)
+        String shellPath = "./src/main/resources/shellScripts/mac/"; //MAC
+        String shellFileName = "gitScript.sh";
+
+        try {
+            String[] command = new String[] {shellPath+shellFileName};
+
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+
+            // 결과를 한줄씩 받아와서 읽어냄.
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            int exitCode = process.waitFor();
+
+
+            System.out.println("\nExited with error code : " + exitCode);
+        }catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
---
+++ bootMachine/src/main/resources/shellScripts/mac/gitScript.sh
@@ -0,0 +1,6 @@
+echo "hello"
+cd ..
+ls
+git status
+git add .
+git commit -m "2022-04-11 커밋하는 쉘 스크립트 작성"
