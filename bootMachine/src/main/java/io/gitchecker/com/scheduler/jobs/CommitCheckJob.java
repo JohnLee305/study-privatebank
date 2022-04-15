531d2ac383098a76a7bfb37a3036631044b4e64c
JohnLee305 <johnathanl305@gmail.com>
2022-04-15 18:44:21 +0900

    2022-04-15 : 스케줄러에서 인스턴스 수정중

--- bootMachine/src/main/java/io/gitchecker/com/scheduler/jobs/CommitCheckJob.java
+++ bootMachine/src/main/java/io/gitchecker/com/scheduler/jobs/CommitCheckJob.java
@@ -5,31 +5,37 @@ import io.gitchecker.com.gitCheckManager.service.GitCheckerService;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 public class CommitCheckJob implements Job {
+
+    private final GitCheckerService gitCheckerService;
+
     @Autowired
-    GitCheckerService gitCheckerService;
+    public  CommitCheckJob(GitCheckerService  gitCheckerService ){
+        this.gitCheckerService = gitCheckerService;
+    }
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         System.out.println("## 잡이 실행되었음.");
 
+        String name = "";
         //매일단위로 실행되는 오토커밋
-        gitCheckerService.autoCommitDaily();
-        String name = jobExecutionContext.getJobDetail().getJobDataMap().get("jobName").toString();
-        try {
-            Job job = jobLocator.getJob(jobName);
-            Map<String, JobParameter> parametersMap = new HashMap<>();
-            parametersMap.put("timestamp", new JobParameter(System.currentTimeMillis()));
-            JobParameters jobParameters = new JobParameters(parametersMap);
-            JobExecution jobExecution = jobLauncher.run(job, jobParameters);
-            log.info("{}_{} was completed successfully", job.getName(), jobExecution.getId());
-        } catch (Exception e) {
-            log.error("Encountered job execution exception!", e);
-        }
-        System.out.println("## Job Name : "+name);
+//        try {
+//            gitCheckerService.autoCommitDaily();
+//            name = jobExecutionContext.getJobDetail().getJobDataMap().get("jobName").toString();
+
+//        } catch (Exception e) {
+//            log.error("Encountered job execution exception!", e);
+//        }finally {
+//            System.out.println("## Job Name : " + name);
+//        }
+
+            gitCheckerService.autoCommitDaily();
+            name = jobExecutionContext.getJobDetail().getJobDataMap().get("jobName").toString();
 
     }
 }
