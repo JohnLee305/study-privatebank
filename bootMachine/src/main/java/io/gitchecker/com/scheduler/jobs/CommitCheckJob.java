91ebb94624e2a87f26d2fa1e5c3455065d8fb076
JohnLee305 <johnathanl305@gmail.com>
2022-04-16 11:34:20 +0900

    2022-04-16 : application,properties 생성

--- bootMachine/src/main/java/io/gitchecker/com/scheduler/jobs/CommitCheckJob.java
+++ bootMachine/src/main/java/io/gitchecker/com/scheduler/jobs/CommitCheckJob.java
@@ -2,40 +2,33 @@ package io.gitchecker.com.scheduler.jobs;
 //실제로 커밋이 되어있는지 확인해주는 잡.
 
 import io.gitchecker.com.gitCheckManager.service.GitCheckerService;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-
+@Slf4j
+@Component
 public class CommitCheckJob implements Job {
 
-    private final GitCheckerService gitCheckerService;
-
     @Autowired
-    public  CommitCheckJob(GitCheckerService  gitCheckerService ){
-        this.gitCheckerService = gitCheckerService;
-    }
+    GitCheckerService gitCheckerService;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
         System.out.println("## 잡이 실행되었음.");
-
-        String name = "";
-        //매일단위로 실행되는 오토커밋
-//        try {
-//            gitCheckerService.autoCommitDaily();
-//            name = jobExecutionContext.getJobDetail().getJobDataMap().get("jobName").toString();
-
-//        } catch (Exception e) {
-//            log.error("Encountered job execution exception!", e);
-//        }finally {
-//            System.out.println("## Job Name : " + name);
-//        }
-
+        try{
             gitCheckerService.autoCommitDaily();
-            name = jobExecutionContext.getJobDetail().getJobDataMap().get("jobName").toString();
+        }catch (NullPointerException ne){
+            log.error("NE!!!!");
+            ne.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
